0074aa8cb586f7f1a2697c501249312b007ff4cd
bruce2233 <864427717@qq.com>
2022-07-25 21:33:32 +0800

    add license comment

--- reactor/reactor.go
+++ reactor/reactor.go
@@ -1,3 +1,25 @@
+// MIT License
+
+// Copyright (c) [2022] [Bruce Zhang]
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package reactor
 
 import (
@@ -40,6 +62,7 @@ const (
 	readWriteEvents = readEvents | writeEvents
 )
 
+//create a new poller
 func OpenPoller() (poller *Poller, err error) {
 	poller = new(Poller)
 	if poller.Fd, err = unix.EpollCreate1(unix.EPOLL_CLOEXEC); err != nil {
@@ -63,9 +86,13 @@ func OpenPoller() (poller *Poller, err error) {
 	// poller.urgentAsyncTaskQueue = queue.NewLockFreeQueue()
 	return
 }
+
+//The proto paramater MUST be "tcp"
+//The addr parameter
 func (mainReactor *MainReactor) Listen(proto, addr string) (fd int, netAddr net.Addr, err error) {
 	fd, netAddr, err = socket.TcpSocket(proto, addr, true)
 	return
+	net.Listen()
 }
 
 func (mainReactor *MainReactor) SetListenerFd(fd int) {
