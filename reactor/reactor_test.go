ccb9603d440795857a7973fd76f4021d15a379e2
bruce2233 <864427717@qq.com>
2022-07-27 00:52:24 +0800

    conn

--- reactor/reactor_test.go
+++ reactor/reactor_test.go
@@ -1,9 +1,16 @@
 package reactor
 
-import "testing"
+import (
+	"net"
+	"testing"
+)
 
 func TestMainRec(t *testing.T) {
 	m := new(MainReactor)
 	m.Init("tcp", "127.0.0.1:9000")
 	m.Loop()
+}
+
+func TestXxx(t *testing.T) {
+	conn := net.Listen()
 }
