5978e9298565681e365addf85536c75c40edf4ab
bruce2233 <864427717@qq.com>
2023-02-12 18:16:00 +0800

    test: HTTP Server based on evnet pass the test!

--- http_test.go
+++ http_test.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/evanphx/wildcat"
 )
@@ -20,7 +21,30 @@ type httpCodec struct {
 func (hs *HttpServer) OnTraffic(c Conn) error {
 	// httpParser := wildcat.HTTPParser{}
 	// println(httpParser.Version)
-	return ErrShutdown
+	hc := c.Context().(*httpCodec)
+	buf, _ := c.Next(-1)
+	// log.Println(hc, buf)
+	_, err := hc.parser.Parse(buf)
+	if err != nil {
+		c.Write([]byte("Internal Server Error"))
+		return ErrClose
+	}
+	hc.appendResponse()
+	bodyLen := int(hc.parser.ContentLength())
+	if bodyLen == -1 {
+		bodyLen = 0
+	}
+	// buf = buf[headerOffset+bodyLen:]
+
+	c.Write(hc.buf)
+	hc.buf = hc.buf[:0]
+	return nil
+}
+
+func (hc *httpCodec) appendResponse() {
+	hc.buf = append(hc.buf, "HTTP/1.1 200 OK\r\nServer: evnet\r\nContent-Type: text/plain\r\nDate: "...)
+	hc.buf = time.Now().AppendFormat(hc.buf, "Mon, 02 Jan 2006 15:04:05 GMT")
+	hc.buf = append(hc.buf, "\r\nContent-Length: 12\r\n\r\nHello World!"...)
 }
 
 func (hs *HttpServer) OnOpen(c Conn) error {
@@ -30,7 +54,7 @@ func (hs *HttpServer) OnOpen(c Conn) error {
 		parser: httpParser,
 	})
 	log.Println("OnOpen: httpCodec ", c.Context().(*httpCodec).parser.TotalHeaders)
-	return ErrClose
+	return nil
 }
 
 func TestHttpBench(t *testing.T) {
@@ -44,4 +68,15 @@ func TestWriteHTTPRequest(t *testing.T) {
 		t.Log(err)
 	}
 	t.Log(resp)
+}
+
+func TestWildCatParse(t *testing.T) {
+	httpParser := wildcat.NewHTTPParser()
+	// str := "GET /example HTTP/1.1\r\nHost: 192.168.87.141:9000"
+	str := "GET /example HTTP/1.1\r\nHost: 192.168.87.141:9000\r\n\r\n"
+	// str := "GET /example HTTP/1.1\r\nHost: 192.168.87.141:90"
+	buf := []byte(str)
+	t.Log(str, len(buf))
+	headerOffset, err := httpParser.Parse(buf)
+	t.Log(headerOffset, err, httpParser.ContentLength())
 }
